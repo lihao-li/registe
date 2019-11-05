243fc9f3b717d1cc7de0eb7c74b1b013c161f381
Administrator <1248117116@qq.com>
2019-11-05 16:00:49 +0900

    Initial commit

---
+++ app/src/androidTest/java/com/example/foolishfan/user_v10/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.example.foolishfan.user_v10;
+
+import android.app.Application;
+
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest {
+    public ApplicationTest() {
+    }
+}
